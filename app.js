dd88129ae08d225ed3f24ae45b4890b803c08585
Gregory Schnese <theschnaz@gmail.com>
2017-08-29 15:35:01 -0400

    adding CORS support for Node

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const path = require('path');
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, 
+  Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
